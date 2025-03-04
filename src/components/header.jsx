6463634751f86f46bcc9a3da3de19e82898577a4
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-03-04 21:16:47 +0100

    initialization root, header, footer and button

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import '../css/header.css';
+// dans le header, on peut utimiser que des balise div,nav,menu,ul et li
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <p>je suis un header</p>
+      </header>
+    </>
+  );
+}
