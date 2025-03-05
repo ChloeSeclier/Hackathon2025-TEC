c09397b122aca36f267f14ce804aa65b95123c8f
Chloe Seclier <seclier.chloe@gmail.com>
2025-03-05 13:34:17 +0100

    break

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import '../css/home.css';
 import Carousel from '../components/Carousel';
+import background from '../assets/background.png'
 
 export default function Home() {
   return (
     <>
-      <div className="home">
-        coucou
-        <p>
+      <div className="home" >
+         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto tenetur
           ducimus sed fugit eius voluptatem consequuntur amet minus vel eum
           praesentium quo, quaerat necessitatibus harum eveniet suscipit minima
@@ -14,9 +14,16 @@ export default function Home() {
           adipisicing elit. Neque ut magnam sed harum facilis eveniet nihil
           fugit fuga, exercitationem ea dolore quia mollitia esse! Veniam
           numquam a velit necessitatibus laudantium?
+          impedit. Explicabo? Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Neque ut magnam sed harum facilis eveniet nihil
+          fugit fuga, exercitationem ea dolore quia mollitia esse! Veniam
+          numquam a velit necessitatibus laudantium?
         </p>
+      </div>     
+        {/* <img src={background} alt="image" /> */}
+        coucou
+  
         <Carousel />
-      </div>
     </>
   );
 }
