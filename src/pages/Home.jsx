989e8d816ec840d3096137eec1efa8e85d34f0c7
Chloe Seclier <seclier.chloe@gmail.com>
2025-03-05 12:24:45 +0100

    add police and image background in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
-import Carousel from "../components/Carousel";
+import '../css/home.css';
+import Carousel from '../components/Carousel';
+
 export default function Home() {
   return (
     <>
-    <div className="home">
-     <Carousel/>
-
-    </div>
+      <div className="home">
+        coucou
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto tenetur
+          ducimus sed fugit eius voluptatem consequuntur amet minus vel eum
+          praesentium quo, quaerat necessitatibus harum eveniet suscipit minima
+          impedit. Explicabo? Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Neque ut magnam sed harum facilis eveniet nihil
+          fugit fuga, exercitationem ea dolore quia mollitia esse! Veniam
+          numquam a velit necessitatibus laudantium?
+        </p>
+        <Carousel />
+      </div>
     </>
   );
 }
