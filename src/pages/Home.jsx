8ea565124d838e8cb1fff401f4faeb551340b5ac
Chloe Seclier <seclier.chloe@gmail.com>
2025-03-05 15:03:43 +0100

    finish home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,68 +5,16 @@ export default function Home() {
   return (
     <>
       <div className="home">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto tenetur
-          ducimus sed fugit eius voluptatem consequuntur amet minus vel eum
-          praesentium quo, quaerat necessitatibus harum eveniet suscipit minima
-          impedit. Explicabo? Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Neque ut magnam sed harum facilis eveniet nihil
-          fugit fuga, exercitationem ea dolore quia mollitia esse! Veniam
-          numquam a velit necessitatibus laudantium? impedit. Explicabo? Lorem
-          ipsum dolor, sit amet consectetur adipisicing elit. Neque ut magnam
-          sed harum facilis eveniet nihil fugit fuga, exercitationem ea dolore
-          quia mollitia esse! Veniam numquam a velit necessitatibus laudantium?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto tenetur
-          ducimus sed fugit eius voluptatem consequuntur amet minus vel eum
-          praesentium quo, quaerat necessitatibus harum eveniet suscipit minima
-          impedit. Explicabo? Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Neque ut magnam sed harum facilis eveniet nihil
-          fugit fuga, exercitationem ea dolore quia mollitia esse! Veniam
-          numquam a velit necessitatibus laudantium? impedit. Explicabo? Lorem
-          ipsum dolor, sit amet consectetur adipisicing elit. Neque ut magnam
-          sed harum facilis eveniet nihil fugit fuga, exercitationem ea dolore
-          quia mollitia esse! Veniam numquam a velit necessitatibus laudantium?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto tenetur
-          ducimus sed fugit eius voluptatem consequuntur amet minus vel eum
-          praesentium quo, quaerat necessitatibus harum eveniet suscipit minima
-          impedit. Explicabo? Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Neque ut magnam sed harum facilis eveniet nihil
-          fugit fuga, exercitationem ea dolore quia mollitia esse! Veniam
-          numquam a velit necessitatibus laudantium? impedit. Explicabo? Lorem
-          ipsum dolor, sit amet consectetur adipisicing elit. Neque ut magnam
-          sed harum facilis eveniet nihil fugit fuga, exercitationem ea dolore
-          quia mollitia esse! Veniam numquam a velit necessitatibus laudantium?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto tenetur
-          ducimus sed fugit eius voluptatem consequuntur amet minus vel eum
-          praesentium quo, quaerat necessitatibus harum eveniet suscipit minima
-          impedit. Explicabo? Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Neque ut magnam sed harum facilis eveniet nihil
-          fugit fuga, exercitationem ea dolore quia mollitia esse! Veniam
-          numquam a velit necessitatibus laudantium? impedit. Explicabo? Lorem
-          ipsum dolor, sit amet consectetur adipisicing elit. Neque ut magnam
-          sed harum facilis eveniet nihil fugit fuga, exercitationem ea dolore
-          quia mollitia esse! Veniam numquam a velit necessitatibus laudantium?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto tenetur
-          ducimus sed fugit eius voluptatem consequuntur amet minus vel eum
-          praesentium quo, quaerat necessitatibus harum eveniet suscipit minima
-          impedit. Explicabo? Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Neque ut magnam sed harum facilis eveniet nihil
-          fugit fuga, exercitationem ea dolore quia mollitia esse! Veniam
-          numquam a velit necessitatibus laudantium? impedit. Explicabo? Lorem
-          ipsum dolor, sit amet consectetur adipisicing elit. Neque ut magnam
-          sed harum facilis eveniet nihil fugit fuga, exercitationem ea dolore
-          quia mollitia esse! Veniam numquam a velit necessitatibus laudantium?
-        </p>
-        coucou
-        
+        <h1>
+          Vous sortez d’un hackathon ?<br />
+          Besoin de déconnecter ?
+        </h1>
+
+        <h2>
+          Laissez-vous tenter par notre jeu pour gagner un moment de détente
+          selon votre besoin.
+        </h2>
+
         <Carousel />
       </div>
     </>
