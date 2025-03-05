0f971dac0cb241bfb6479f5f27a081300e0d7c6d
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-03-05 13:48:03 +0100

    done

--- src/pages/lots.jsx
+++ src/pages/lots.jsx
@@ -7,17 +7,17 @@ export default function Lots() {
 
       <ul>
         <li>
-          <h2>Lot 1 : Séance Yoga</h2>{' '}
+          <h2>Lot 1 : Séance Yoga</h2>
           <img src="src\assets\image-lot-1.jpg" alt="" />
         </li>
 
         <li>
-          <h2>Lot 2 : Espace détente</h2>{' '}
+          <h2>Lot 2 : Espace détente</h2>
           <img src="src\assets\image-lot-2.jpg" alt="" />
         </li>
 
         <li>
-          <h2>Lot 3 : Séjour insolite </h2>{' '}
+          <h2>Lot 3 : Séjour insolite </h2>
           <img src="src\assets\image-lot-3.jpg" alt="" />
         </li>
       </ul>
