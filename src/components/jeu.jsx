de1be75cae2e467a12ca43097a3adca4094f3481
Chloe Seclier <seclier.chloe@gmail.com>
2025-03-05 20:21:15 +0100

    display all question

--- src/components/jeu.jsx
+++ src/components/jeu.jsx
@@ -1,27 +1,52 @@
+import { useState } from 'react';
 import { listQuestion } from '../objet/listQuestion';
 
 export default function Jeu() {
-  const indexQuestion = 0;
+  const [indexQuestion, setIndexQuestion] = useState(0);
   const question = listQuestion[indexQuestion];
-  console.log(question.reponses);
+  const [score, setScore] = useState(0);
+  const [answerChoiced, setAnswerChoiced] = useState(0);
+
+  function handleValider() {
+    setScore(score + answerChoiced);
+    setIndexQuestion(indexQuestion + 1);
+    setAnswerChoiced(0);
+  }
+  console.log(indexQuestion);
 
   return (
     <div className="container-jeu">
-      <div className="question">{listQuestion[indexQuestion].question}</div>
-      {question.typeRep === 'boolean' ? (
-        //Display les 2 boutons pour vrai ou faux
+      {listQuestion.length > indexQuestion  ? (
+        <>
+          <div className="question">{listQuestion[indexQuestion].question}</div>
+          {question.typeRep === 'boolean' ? (
+            //Display les 2 boutons pour vrai ou faux
 
-        question.reponses.map((answer, index) => (
-          <div key={index} className="button-boolean">
-            <input type="radio" id={answer.reponse} name="jeu" value={answer.point} checked />
-            <label htmlFor={answer.reponse}>{answer.reponse}</label>
-          </div>
-        ))
+            question.reponses.map((answer, index) => (
+              <div key={index} className="button-boolean">
+                <input
+                  type="radio"
+                  id={answer.reponse}
+                  name="jeu"
+                  value={answer.point}
+                  onChange={() => setAnswerChoiced(answer.point)}
+                />
+                <label htmlFor={answer.reponse}>{answer.reponse}</label>
+              </div>
+            ))
+          ) : (
+            //Display l'échelle
+            <p>non</p>
+          )}
+          <button type="button" onClick={handleValider}>
+            Valider
+          </button>
+
+          <p>Score : {score}</p>
+        </>
       ) : (
-        //Display l'échelle
-        <p>non</p>
+        <p>afficher score</p>
       )}
-      <button type="button" onClick={}>Valider</button>
     </div>
   );
 }
