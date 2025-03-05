3ba5fd55bbc9f1a849c2f26069c44f71da00e7b7
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-03-05 18:51:39 +0100

    burger

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,38 @@
 import '../css/header.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 // dans le header, on peut utimiser que des balise div,nav,menu,ul et li
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <header className="header">
         <Link to="/">
           <img src="src\assets\logo.png" alt="" />
         </Link>
-        <ul>
-          <li>Gagne des Vacances</li>
-          <li>
-            <Link to="/Lots" className="cadeaux-link">     
-              Cadeaux
-            </Link>
-          </li>
-          <li>Règlement</li>
-          <li>Connexion</li>
-        </ul>
+        
+          <div
+            className="burger"
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <ul className={menuOpen ? 'open' : ''}>
+            <li>Gagne des Vacances</li>
+            <li>
+              <Link to="/Lots" className="cadeaux-link">
+                Cadeaux
+              </Link>
+            </li>
+            <li>Règlement</li>
+            <li>Connexion</li>
+          </ul>
+        
       </header>
     </>
   );
