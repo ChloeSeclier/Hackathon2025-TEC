b64336ab0d7b57ad58479e6d173c12cb9b91d7b4
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-03-05 20:59:03 +0100

    scale

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
           selon votre besoin.
         </h2>
         <Carousel />
-        <Link to="/jeu">TENTER MA CHANCE</Link>
+        <Link to="/regles-jeu">TENTER MA CHANCE</Link>
       </div>
     </>
   );
