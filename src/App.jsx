895611112ad5db2748442d751ae3e256456efe49
Chloe Seclier <seclier.chloe@gmail.com>
2025-03-10 10:48:09 +0100

    change majuscule

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { ScaleProvider } from './components/scaleContext';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Footer from './components/footer';
+import Header from './components/header';
 import './App.css';
 
 function App() {
