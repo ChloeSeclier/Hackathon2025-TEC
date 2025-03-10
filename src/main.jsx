49b9252007dfcb738b63025c8307ded481659f65
Chloe Seclier <seclier.chloe@gmail.com>
2025-03-10 10:39:55 +0100

    add in router a sentence

--- src/main.jsx
+++ src/main.jsx
@@ -15,53 +15,58 @@ import Mentions from './pages/mentions';
 import Contact from './pages/contact';
 import Reglement from './pages/reglement';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <App />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/regles-jeu',
+          element: <ReglesJeu />,
+        },
+
+        {
+          path: '/reglement',
+          element: <Reglement />,
+        },
+
+        {
+          path: '/jeu',
+          element: <Jeu />,
+        },
+        {
+          path: '/lots',
+          element: <Lots />,
+        },
+        {
+          path: '/contact',
+          element: <Contact />,
+        },
+        {
+          path: '/mentions',
+          element: <Mentions />,
+        },
+        {
+          path: '/politique',
+          element: <Politique />,
+        },
+        { path: '/detail/:id', element: <DetailPage /> },
+        { path: '/detail/:score', element: <DetailPage /> },
+        {
+          path: '*',
+          element: <Error404 />,
+        },
+      ],
+    },
+  ],
   {
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/regles-jeu',
-        element: <ReglesJeu />,
-      },
-
-      {
-        path: '/reglement',
-        element: <Reglement />,
-      },
-
-      {
-        path: '/jeu',
-        element: <Jeu />,
-      },
-      {
-        path: '/lots',
-        element: <Lots />,
-      },
-      {
-        path: '/contact',
-        element: <Contact />,
-      },
-      {
-        path: '/mentions',
-        element: <Mentions />,
-      },
-      {
-        path: '/politique',
-        element: <Politique />,
-      },
-      { path: '/detail/:id', element: <DetailPage /> },
-      { path: '/detail/:score', element: <DetailPage /> },
-      {
-        path: '*',
-        element: <Error404 />,
-      },
-    ],
+    basename: import.meta.env.BASE_URL,
   },
-]);
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
